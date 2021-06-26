34fab590ab2645b04f15f0f0c5a48d72f725473d
FreddyCastuera <freddycastuera@outlook.es>
2021-06-26 00:21:50 -0600

    agregue los ejercicios a resolver

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*1 crear una funcion que cuente el numero de caracteres de 
+    una cadena de texto P.E: f("hola mundo") = 10*/
+/*2 crear una funcion que te devuelva el texto recortado segun 
+    el numero pasado como segundo parametro P.E: f("hola mundo",6) = "hola m" */
+/*3 crear una funcion que dada una string pasada como primer parametro 
+    te devuelva un arreglo con las palabras separadas segun el caracter
+    pasado como segundo parametro P.E: f("hola*como*estas","*") = ["hola","como","estas"]*/
+/*4 crear una funcion que dado un string te devuelva otro string repetido segun
+    el segundo parametro P.E: f("oso",5) = "oso oso oso oso oso"*/
+/*5 crea una funcion que invierta los caracteres de una cadena de texto 
+    P.E: f("barbacoa") = "aocabrab" */ /*BONUS chechar si una palabra es un palindromo*/
+/*6 crear una funcion que cuente el numero de veces que se reptie una palabra en un string
+    P.E: f("hola mundo adios mundo","mundo") = 2 */
+/*7 crear una funcion que evalue si un numero es capicua (se lee igual en ambos sentidos)
+    P.E: f(1234321) = true*/
+/*8 crear una funcion que calcule el factorial de un numero*/
+/*9  crear una funcion que calcule n-numeros de fibonacci*/
+/*10 crear una funcion que determine si un numero es primo o nel*/
+/*11 crear una funcion que convierta numeros de basa binaria a base decimal*/
+/*12 crear una funcion que dado un arreglo de numeros te regrese un arreglo con 
+    los numeros elevados al cuadrado*/
+/*13 crear una funcion que dado un arreglo con numeros te devuelva el mayor y el menor*/
+/*14 crear una funcion que dado un arreglo con numeros devuevla un objecto con
+    los numeros pares en un arreglo y los impares en otro 
+    P.E: f([1,2,3,4,5,6,7,8]) = {pares:[2,4,6,8], impar:{1,3,5,7}}*/
+/*15 crear una funcion que dado un arreglo de numeros devuelva un objecto con dos
+arreglos, uno ordenado de manera ascendente y el otro descendente 
+    P.E: f([3,2,5,1,6]) = {ascendente:[1,2,3,4,5,6], descendete:[6,5,4,3,2,1]}*/
+/*16  crear una funcion que dado un arreglo elimine los elementos duplciados*/
+/*17 crear una funcion que dado un arreglo de elementos calcule la media aritmetica,media geometrica, variaza y desviazion estandar*/
+
+/*problemas tentativos*/
+/*. Integer Reversal*/
+/* Anagrams */
+/*Array Chunking
+Given an array and a size, split the array items into a list of arrays of the given size.*/
+/*Reverse Array*/
+/*Caesar Cipher
+Given a phrase, substitute each character by shifting it up or down the alphabet by a given integer. If necessary, the shifting should wrap around back to the beginning or end of the alphabet.*/
+/* Pyramid
+For a given number of levels, print out a “pyramid” using hashes and spaces.*/
